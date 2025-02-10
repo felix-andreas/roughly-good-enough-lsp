5c967a09067cf7346c74fce167d6018dd32a4841
Felix Andreas <felix.andreas95@googlemail.com>
2025-02-10 01:46:40 +0100

    fix parameters

--- src/format.rs
+++ src/format.rs
@@ -6,150 +6,6 @@ use {
     tree_sitter::{Node, Tree, TreeCursor},
 };
 
-fn format(tree: &Tree, rope: &Rope) -> Option<String> {
-    let mut cursor = tree.walk();
-    // let mut stack = vec![];
-    // todo: initizalize with cap
-    let mut result = String::new();
-    let mut i = 0;
-    loop {
-        i += 1;
-        if i > 10000 {
-            log::error!("reached recursion limit!");
-            return None;
-        }
-        let node = cursor.node();
-
-        let start = match node.kind() {
-            "argument" => "argument",
-            "arguments" => "arguments",
-            "binary_operator" => "binary_operator",
-            "braced_expression" => "braced_expression",
-            "call" => "call",
-            "complex" => "complex",
-            "extract_operator" => "extract_operator",
-            "float" => "float",
-            "for_statement" => "for_statement",
-            "function_definition" => "function_definition",
-            "if_statement" => "if_statement",
-            "integer" => "integer",
-            "na" => "na",
-            "namespace_operator" => "namespace_operator",
-            "parameter" => "parameter",
-            "parameters" => "parameters",
-            "parenthesized_expression" => "parenthesized_expression",
-            "program" => "program",
-            "repeat_statement" => "repeat_statement",
-            "string" => "string",
-            "string_content" => "string_content",
-            "subset" => "subset",
-            "subset2" => "subset2",
-            "unary_operator" => "unary_operator",
-            "while_statement" => "while_statement",
-            "!" => "!",
-            "!=" => "!=",
-            "\"" => "\"",
-            "$" => "$",
-            "&" => "&",
-            "&&" => "&&",
-            "'" => "'",
-            "(" => "(",
-            ")" => ")",
-            "*" => "*",
-            "**" => "**",
-            "+" => "+",
-            "-" => "-",
-            "->" => "->",
-            "->>" => "->>",
-            "/" => "/",
-            ":" => ":",
-            "::" => "::",
-            ":::" => ":::",
-            ":=" => ":=",
-            "<" => "<",
-            "<-" => "<-",
-            "<<-" => "<<-",
-            "<=" => "<=",
-            "=" => "=",
-            "==" => "==",
-            ">" => ">",
-            ">=" => ">=",
-            "?" => "?",
-            "@" => "@",
-            "L" => "L",
-            "NA" => "NA",
-            "NA_character_" => "NA_character_",
-            "NA_complex_" => "NA_complex_",
-            "NA_integer_" => "NA_integer_",
-            "NA_real_" => "NA_real_",
-            "[" => "[",
-            "[[" => "[[",
-            "\\" => "\\",
-            "]" => "]",
-            "]]" => "]]",
-            "^" => "^",
-            "break" => "break",
-            "comma" => "comma",
-            "comment" => "comment",
-            "dot_dot_i" => "dot_dot_i",
-            "dots" => "dots",
-            "else" => "else",
-            "escape_sequence" => "escape_sequence",
-            "false" => "false",
-            "for" => "for",
-            "function" => "function",
-            "i" => "i",
-            "identifier" => "identifier",
-            "if" => "if",
-            "in" => "in",
-            "inf" => "inf",
-            "nan" => "nan",
-            "next" => "next",
-            "null" => "null",
-            "repeat" => "repeat",
-            "return" => "return",
-            "special" => "special",
-            "true" => "true",
-            "while" => "while",
-            "{" => "{",
-            "|" => "|",
-            "|>" => "|>",
-            "||" => "||",
-            "}" => "}",
-            "~" => "~",
-            unknown => {
-                log::error!("UNKNOWN NODE KIND: {unknown}");
-                return None;
-            }
-        };
-
-        result.push_str(start);
-
-        if cursor.goto_first_child() {
-            println!("got to first child");
-            continue;
-        }
-
-        if cursor.goto_next_sibling() {
-            println!("got to first child");
-            continue;
-        }
-
-        loop {
-            if !cursor.goto_parent() {
-                println!("no parent");
-                dbg!(&result);
-                return Some(result);
-            }
-
-            if cursor.goto_next_sibling() {
-                println!("no next sibling");
-                break;
-            }
-        }
-    }
-}
-
 #[derive(Error, Debug)]
 pub enum FormatError {
     #[error("The node of type {kind} has missing children {raw}")]
@@ -163,7 +19,7 @@ pub enum FormatError {
     },
 }
 
-fn format_recursive(node: Node, rope: &Rope) -> Result<String, FormatError> {
+fn format(node: Node, rope: &Rope) -> Result<String, FormatError> {
     if node.is_missing() {
         let parent = node.parent().unwrap();
         return Err(FormatError::Missing {
@@ -194,7 +50,7 @@ fn format_recursive(node: Node, rope: &Rope) -> Result<String, FormatError> {
             .map(f)
             .collect::<Result<Vec<String>, FormatError>>()
     };
-    let fmt = |node: Node| format_recursive(node, rope);
+    let fmt = |node: Node| format(node, rope);
 
     // DEBUG
     // dbg!(node.child(0));
@@ -207,38 +63,23 @@ fn format_recursive(node: Node, rope: &Rope) -> Result<String, FormatError> {
             let maybe_name = field_optional("name");
             let maybe_value = field_optional("value");
             match (maybe_name, maybe_value) {
-                (Some(name), Some(value)) => format!("{name} = {value}"),
-                (None, Some(value)) => format!("{value}"),
-                (Some(name), None) => format!("{name}"),
+                (Some(name), Some(value)) => format!("{} = {}", fmt(name)?, fmt(value)?),
+                (None, Some(value)) => format!("{}", fmt(value)?),
+                (Some(name), None) => format!("{}", fmt(name)?),
                 (None, None) => format!(""),
             }
         }
         "arguments" => {
             let mut cursor = node.walk();
-            let mut multiline = None;
             let open = field("open")?;
             let close = field("close")?;
-            let arguments = node
-                .children_by_field_name("argument", &mut cursor)
-                .map(|argument| {
-                    let (start, end) = (argument.start_position().row, argument.end_position().row);
-                    multiline = Some(multiline.map_or((start, end), |(s, e)| {
-                        (usize::min(s, start), usize::max(e, end))
-                    }));
-                    format_recursive(argument, rope).unwrap()
-                })
-                .collect::<Vec<String>>();
+            let arguments = fields("argument", &mut cursor, fmt)?;
             let seperator = if open.start_position().row == close.end_position().row {
                 ", "
             } else {
                 ",\n"
             };
             arguments.join(seperator)
-            // match multiline {
-            //     None => "".into(),
-            //     Some((start, end)) if start == end => arguments.join(", "),
-            //     _ => arguments.join(",\n"),
-            // }
         }
         "binary_operator" => {
             format!(
@@ -359,24 +200,15 @@ fn format_recursive(node: Node, rope: &Rope) -> Result<String, FormatError> {
         }
         "parameters" => {
             let mut cursor = node.walk();
-
-            let mut multiline = None;
-            let parameters = node
-                .children_by_field_name("parameter", &mut cursor)
-                .map(|parameter| {
-                    let (start, end) =
-                        (parameter.start_position().row, parameter.end_position().row);
-                    multiline = Some(multiline.map_or((start, end), |(s, e)| {
-                        (usize::min(s, start), usize::max(e, end))
-                    }));
-                    fmt(parameter).unwrap()
-                })
-                .collect::<Vec<String>>();
-            match multiline {
-                None => "".into(),
-                Some((start, end)) if start == end => parameters.join(", "),
-                _ => parameters.join(",\n"),
-            }
+            let open = field("open")?;
+            let close = field("close")?;
+            let parameters = fields("parameter", &mut cursor, fmt)?;
+            let seperator = if open.start_position().row == close.end_position().row {
+                ", "
+            } else {
+                ",\n"
+            };
+            parameters.join(seperator)
         }
         "parenthesized_expression" => {
             let open = field("open")?;
@@ -473,14 +305,6 @@ fn format_recursive(node: Node, rope: &Rope) -> Result<String, FormatError> {
     })
 }
 
-// fn format_binary_operator(: &Tree, rope: &Rope) -> Option<String> {
-
-// }
-
-// fn traverse(tree: &Tree) -> String {
-//     match tree.kind {}
-// }
-
 #[cfg(test)]
 mod test {
     use {super::*, crate::index, indoc::indoc};
@@ -491,7 +315,7 @@ mod test {
 
         // DEBUG
         dbg!(tree.root_node().to_sexp());
-        format_recursive(tree.root_node(), &Rope::from_str(text)).unwrap()
+        format(tree.root_node(), &Rope::from_str(text)).unwrap()
     }
 
     #[test]
@@ -519,7 +343,6 @@ mod test {
         insta::assert_snapshot!(fmt(indoc! {r#"
             { 1L;2}
         "#}));
-        // assignments
         insta::assert_snapshot!(fmt(indoc! {r#"
             {
                 foo
@@ -535,11 +358,11 @@ mod test {
     #[test]
     fn test_call() {
         insta::assert_snapshot!(fmt(indoc! {r#"
-            list  (a = 1, b= 2)
+            list  (a = 1, b= 2L ,c =3i  )
         "#}));
         insta::assert_snapshot!(fmt(indoc! {r#"
             list  (a = 1,
-             b= 2)
+             b= 2L)
         "#}));
     }
 
@@ -549,13 +372,12 @@ mod test {
             function(a, b= "foo") {}
         "#}));
         insta::assert_snapshot!(fmt(indoc! {r#"
-            function( a
-            , b=  "foo") {}
+            function(a,
+            b=  "foo") {}
         "#}));
         insta::assert_snapshot!(fmt(indoc! {r#"
             (\(a, b) a *  b)(2, 3)
         "#}));
-        // TODO: make this work
         insta::assert_snapshot!(fmt(indoc! {r#"
             function(
                 a , b=  "foo") {}
